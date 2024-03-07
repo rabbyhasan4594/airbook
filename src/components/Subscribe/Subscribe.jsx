97ce2ac110303bfef87fbbd39c3d45478073b879
rabbyhasan <rabbyhasan4594@gmail.com>
2024-03-07 14:28:22 +0600

    subscribe added

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Subscribe = () => {
     return (
-        <div className=''>
-            <div><img src="https://i.ibb.co/Sn2KsyB/Isolation-Mode.png" alt="" /></div>
-            <div><h1>hi</h1></div>
+        <div className='lg:px-32 lg:mb-40 mb-4'>
+            <div className='flex justify-between items-center pt-24 '>
+                <div className='lg:pb-5'>
+                    <img src="https://i.ibb.co/Sn2KsyB/Isolation-Mode.png" alt="" className='w-[386px] h-[394px]'/></div>
+                <div className=''>
+                    <h1 className='text-4xl font-bold lg:w-2/3'>Subscribe to our
+                        Newsletter!</h1>
+                    <p className='text-xl font-medium lg:2/3 text-blue-500 my-3'>Subscribe to our newsletter and stay
+                    </p>
+                    <div className='mb-2'>
+                    <input type="email" placeholder="Your Email" className="input input-bordered w-full " />
+                    </div>
+                    <button className='w-full py-2 text-xl font-medium bg-yellow-500 rounded-lg'>Subscribe</button>
+                </div>
+            </div>
         </div>
     );
 };
